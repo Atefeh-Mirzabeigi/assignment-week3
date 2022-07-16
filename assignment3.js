379043062c0fa31f23be1bde70b565a4d62a7e58
Atefeh <68872742+Atefeh-Mirzabeigi@users.noreply.github.com>
2022-07-16 13:54:26 +0200

    add spell check to player function

--- assignment3.js
+++ assignment3.js
@@ -1,31 +1,45 @@
 const computerPlay = () => {
   let myArray = ["paper", "rock", "scissors"];
-  let randomWorld = Math.floor(Math.random() * 3);
-  return myArray[randomWorld];
+  let randomWord = Math.floor(Math.random() * 3);
+  return myArray[randomWord];
 };
 
+const playerSelection = () => {
+  let player = prompt("Enter rock, paper or scissors").toLowerCase();
+  if (player !== "rock" && player !== "scissors" && player !== "paper") {
+    alert("please choose rock, paper or scissors");
+  } else {
+    return player;
+  }
+};
 //let playerSelection = prompt();
 
 const playRound = () => {
-  let player = prompt("Enter rock, paper or scissors").toLowerCase();
+  let player = playerSelection();
   let computer = computerPlay();
 
   if (player == "rock" && computer == "paper") {
+    console.log("You lose! paper beats rock");
     return `You Loooose`;
   }
   if (player == "paper" && computer == "rock") {
+    console.log("You win! paper beats rock");
     return `You win`;
   }
   if (player == "rock" && computer == "scissors") {
+    console.log("You win! rock beats scissors");
     return `You win`;
   }
   if (player == "scissors" && computer == "paper") {
+    console.log("You win! scissors beats paper");
     return `You win`;
   }
   if (player == "paper" && computer == "scissors") {
+    console.log("You lose! scissors beats paper");
     return `You Loooose`;
   }
   if (player == "scissors" && computer == "rock") {
+    console.log("You lose! rock beats scissors");
     return `You Loooose`;
   } else {
     return "Oh..nobody wins";
